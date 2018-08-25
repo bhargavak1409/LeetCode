930bde31125f78ccf547aeca86e1073f93992dfa
Bhargava Kumar <bk2668@columbia.edu>
2018-08-24 21:26:53 -0400

    Update add-two-numbers.py
    The previous code does not work for [9,9,9] and [9,9,9]

--- Python/add-two-numbers.py
+++ Python/add-two-numbers.py
@@ -1,24 +1,11 @@
-# Time:  O(n)
-# Space: O(1)
-#
-# You are given two linked lists representing two non-negative numbers.
-# The digits are stored in reverse order and each of their nodes contain
-# a single digit.
-# Add the two numbers and return it as a linked list.
-#
-# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-# Output: 7 -> 0 -> 8
-#
-
-
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 
-class Solution(object):
+class Solution():
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
@@ -29,14 +16,17 @@ class Solution(object):
         current, carry = dummy, 0
 
         while l1 or l2:
-            val = carry
+            val = 0
             if l1:
                 val += l1.val
                 l1 = l1.next
             if l2:
                 val += l2.val
                 l2 = l2.next
+                        
+            val = val+carry
             carry, val = divmod(val, 10)
+            
             current.next = ListNode(val)
             current = current.next
 
@@ -44,3 +34,4 @@ class Solution(object):
             current.next = ListNode(1)
 
         return dummy.next
+    
